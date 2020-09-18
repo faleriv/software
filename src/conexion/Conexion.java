1168a544cd2d9fafa563cc84641efe496c9d2dad
Diego <Diego@DESKTOP-KL50MP5>
2020-09-18 12:02:32 -0500

    Ajustes de Tablas

--- src/conexion/Conexion.java
+++ src/conexion/Conexion.java
@@ -33,8 +33,8 @@ public class Conexion {
         }
         try
         {
-            contacto = DriverManager.getConnection(url,"sa","Diego2420347");//Diego
-            //contacto = DriverManager.getConnection(url,"sa","fielEsJesus");//Vale
+            //contacto = DriverManager.getConnection(url,"sa","Diego2420347");//Diego
+            contacto = DriverManager.getConnection(url,"sa","fielEsJesus");//Vale
             //contacto = DriverManager.getConnection(url,"sa","12345");//Jose
         }
        catch (SQLException e)
