7f6263c5fdb0b8c3385253e28a7d8911cd2a965e
Diego <Diego@DESKTOP-KL50MP5>
2020-09-17 20:10:48 -0500

    Modifique que no sea editable tabla cliente

--- src/conexion/Conexion.java
+++ src/conexion/Conexion.java
@@ -19,9 +19,9 @@ public class Conexion {
     
     public static Connection getConexion()
     {
-        //String url ="jdbc:sqlserver://DESKTOP-KL50MP5:1433;databaseName=sf_si";
+        String url ="jdbc:sqlserver://DESKTOP-KL50MP5:1433;databaseName=sf_si";//Diego
          //String url ="jdbc:sqlserver://DESKTOP-MR4QM66:1433;databaseName=sf_si";//Vale
-         String url ="jdbc:sqlserver://DESKTOP-4S9GK79:1433;databaseName=sf_si";//jose
+         //String url ="jdbc:sqlserver://DESKTOP-4S9GK79:1433;databaseName=sf_si";//jose
         try
         {
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
